a8cbe9f233fcad3181222f955924c066f5c98855
Steven Michael <ssmichael@gmail.com>
2022-01-10 10:34:51 -0500

    add util.js

--- test/coordconversion-test.js
+++ test/coordconversion-test.js
@@ -15,6 +15,7 @@ tape('Greenwich Mean Sidereal Time', (test) => {
     // Example 3-5 from Vallado
     let date = new Date(Date.UTC(1992, 7, 20, 12, 14))
     let gmst_ = ak.gmst(date.jd(Date.timescale.UTC))
+    console.log(gmst_ * ak.rad2deg)
     test.assert(Math.abs(gmst_ * 180.0 / Math.PI / -207.4212 - 1) < 1.0e-6,
         'Vallado example 3-5, finding gmst')
 
