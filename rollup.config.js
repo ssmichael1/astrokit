bda6f5f4ddb239b960cffcf39fc8feedd867c6c0
Steven Michael <ssmichael@gmail.com>
2022-01-13 13:13:39 -0500

    works now with nodeJS

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { readFileSync } from "fs";
+import * as meta from "./package.json";
+
+// Extract copyrights from the LICENSE.
+const copyright = readFileSync("./LICENSE", "utf-8")
+    .split(/\n/g)
+    .filter(line => /^Copyright\s+/.test(line))
+    .map(line => line.replace(/^Copyright\s+/, ""))
+    .join(", ");
+
+export default {
+    input: "src/index.js",
+    output: {
+        file: `dist/${meta.name}.js`,
+        name: "ak",
+        format: "umd",
+        indent: false,
+        extend: true,
+        banner: `// v${meta.version} Copyright ${copyright}`
+    },
+};
+
